d559a9da102d51d29e7ac905ccde21180f7adf05
Huiye Zhuang <huiye@cs.ualberta.ca>
2017-10-11 18:53:44 -0600

    lab

--- app/src/androidTest/java/ca/ualberta/cs/lonelytwitter/TweetListTest.java
+++ app/src/androidTest/java/ca/ualberta/cs/lonelytwitter/TweetListTest.java
@@ -13,18 +13,39 @@ public class TweetListTest extends ActivityInstrumentationTestCase2 {
     }
 
     public void testAddTweet() {
-        assertTrue(Boolean.FALSE);
+        TweetList tweets = new TweetList();
+        Tweet tweet = new NormalTweet("adding test");
+        tweets.add(tweet);
+        assertTrue(tweets.hasTweet(tweet));
     }
 
     public void testDelete() {
-        assertTrue(Boolean.FALSE);
+        TweetList tweets = new TweetList();
+        Tweet tweet = new NormalTweet("test");
+        tweets.add(tweet);
+        tweets.delete(tweet);
+        assertFalse(tweets.hasTweet(tweet));
     }
 
     public void testHasTweet() {
         TweetList list = new TweetList();
         Tweet tweet = new NormalTweet("test");
         list.add(tweet);
-        assertTrue(list.contains(tweet));
+        assertTrue(list.hasTweet(tweet));
+    }
 
+    public void testGetTweets() {
+        TweetList list = new TweetList();
+        Tweet tweet = new NormalTweet("test");
+        list.add(tweet);
+        Tweet returnedTweet = list.getTweets(0);
+        assertEquals(returnedTweet.getMessage(), tweet.getMessage());
+    }
+
+    public void testGetcount() {
+        TweetList list = new TweetList();
+        Tweet tweet = new NormalTweet("test");
+        list.add(tweet);
+        assertEquals(list.getCount(),1);
     }
 }
